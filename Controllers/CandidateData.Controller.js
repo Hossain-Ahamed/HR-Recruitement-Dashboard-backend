d1832c0407a3ed5c0a1be3b5c4c63a7e6c917e8b
Hossain-Ahamed <hossainahamed6872@gmail.com>
2024-03-19 04:11:16 +0600

    candidate all

--- Controllers/CandidateData.Controller.js
+++ Controllers/CandidateData.Controller.js
@@ -16,7 +16,7 @@ const getAllCandidates = async (req, res) => {
       });
     }
 
-    if (status && status !== "All") {
+    if (status && status !== "all") {
       pipeline.push({
         $match: { status: { $regex: new RegExp(status, "i") } },
       });
