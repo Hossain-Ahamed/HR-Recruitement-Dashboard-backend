fe98d871ec092479ff4c19062483c27e23e81f92
Hossain Ahamed <hossainahamed6872@gmail.com>
2024-03-19 22:27:55 +0600

    vercel route

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ const corsOptions = {
         'http://localhost:5174',
         'http://localhost:5175',
         'http://localhost:3000',
-        'https://hr-recruitement-dashboard-backend.vercel.app/',
-        'https://hr-recruitement-dashboard.vercel.app/'
+        'https://hr-recruitement-dashboard-backend.vercel.app',
+        'https://hr-recruitement-dashboard.vercel.app'
     ],
     credentials: true,
 };
